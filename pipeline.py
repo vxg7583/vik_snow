b2dc0008aacb607938d890905c0fd5933d7596fc
Vikrant Gawde <vikrant.gawde10@gmail.com>
2025-04-08 14:23:03 -0700

    Create pipeline.py

---
+++ pipeline.py
@@ -0,0 +1,11 @@
+import subprocess
+def run_script(script_name):
+    try:
+        subprocess.run(["python", script_name], check=True)
+        print(f"Successfully executed {script_name}")
+    except subprocess.CalledProcessError as e:
+        print(f"Error executing {script_name}: {e}")
+if __name__ == "__main__":
+    run_script("data_transform.py")
+    run_script("train.py")
+    run_script("eval.py")
